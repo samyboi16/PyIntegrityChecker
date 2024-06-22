53227f4f3b03e067a0b111918ec10ceaf94902bd
samyboi16 <95954618+samyboi16@users.noreply.github.com>
2024-06-22 14:50:24 +0400

    Update File_integrity_checker.py

--- File_integrity_checker.py
+++ File_integrity_checker.py
@@ -5,38 +5,40 @@ from pathlib import Path
 from tkinter import ttk, filedialog
 import tkinter as tk
 from ttkthemes import ThemedStyle
+#####################################---PyCode---#########################################
 
 
-def Sha256_HashCalc(file_name):
     
+def open_file(): 
+    file_path = filedialog.askopenfilename()
+    filename_label.config(text="Selected File: " + os.path.basename(file_path))
+    return file_path
+
+def Sha256_HashCalc():
+    file_path=open_file()
     buffer_size = 65536
     sha = hashlib.sha256()
     
-    with open(file_name,'rb') as file:
+    with open(file_path,'rb') as file:
         while True:
             data = file.read(buffer_size)
             if not data:
                 break
             sha.update(data)
         return sha.hexdigest()
-    
 
 def storehash(hash_value):
     hash_file_path = os.path.join('baseline', f"{file_name}.hash")
     with open(hash_file_path, 'w') as hash_file:
         hash_file.write(hash_value)
 
-myfilepath=filedialog.askopenfilename()
-
-with open(myfilepath,'r') as file:
-    file_name=file.read()
-print(file_name)
 
 
 #############################---GUI---####################################
+
 #Create the main tkinter window
 root = tk.Tk() 
-root.title("Secure File Encryption")
+root.title("Py File Integrity Checker")
 root.config(bg="#f2f2f2")  
 
 #Set the theme for ttk widgets
@@ -44,17 +46,20 @@ style = ThemedStyle(root)
 style.set_theme("vista")  
 
 #UI title
-label = ttk.Label(root, text="Secure File Encryption", font=("Segoe UI", 20), foreground="#3498db", background="#f2f2f2")
+label = ttk.Label(root, text="Py File Integrity Checker", font=("Segoe UI", 20), foreground="#3498db", background="#f2f2f2")
 label.pack(pady=20)
 
-#Create and pack widgets for the user interface
-login_frame = ttk.Frame(root, padding=(5, 5, 5, 5)) 
-login_frame.pack()
+browse = ttk.Button(text="Browse", command=open_file, style="Rounded.TButton", cursor="hand2")
+browse.pack()
 
 style.configure("Rounded.TButton", borderwidth=0, relief="flat", padding=10, font=("Segoe UI", 10)) 
 
-root.mainloop()
+#Create a label to display the selected file status
+filename_label = ttk.Label(root, text="Selected File: None", font=("Segoe UI", 12), foreground="black", background="#D3D3D3") 
+filename_label.pack(pady=10)
 
-hash_file=Sha256_HashCalc(myfilepath)
-file_name=os.path.basename(myfilepath)
+root.mainloop()
+######################################################################k
+hash_file=Sha256_HashCalc()
+#file_name=os.path.basename(myfilepath)
 storehash(hash_file)
