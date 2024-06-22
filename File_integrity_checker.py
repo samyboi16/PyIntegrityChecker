b7e6e2985db912fc461030209ec85923cf62c202
samyboi16 <95954618+samyboi16@users.noreply.github.com>
2024-06-22 12:39:55 +0400

    Update File_integrity_checker.py

--- File_integrity_checker.py
+++ File_integrity_checker.py
@@ -3,11 +3,13 @@ import shutil
 import hashlib
 from pathlib import Path
 from tkinter import ttk, filedialog
+import tkinter as tk
+from ttkthemes import ThemedStyle
 
 
 def Sha256_HashCalc(file_name):
-    # BUF_SIZE is totally arbitrary, change as per your requirement
-    buffer_size = 65536  # 65536 lets read stuff in 64kb chunks!
+    
+    buffer_size = 65536
     sha = hashlib.sha256()
     
     with open(file_name,'rb') as file:
@@ -30,6 +32,29 @@ with open(myfilepath,'r') as file:
     file_name=file.read()
 print(file_name)
 
-has_file=Sha256_HashCalc(myfilepath)
+
+#############################---GUI---####################################
+#Create the main tkinter window
+root = tk.Tk() 
+root.title("Secure File Encryption")
+root.config(bg="#f2f2f2")  
+
+#Set the theme for ttk widgets
+style = ThemedStyle(root) 
+style.set_theme("vista")  
+
+#UI title
+label = ttk.Label(root, text="Secure File Encryption", font=("Segoe UI", 20), foreground="#3498db", background="#f2f2f2")
+label.pack(pady=20)
+
+#Create and pack widgets for the user interface
+login_frame = ttk.Frame(root, padding=(5, 5, 5, 5)) 
+login_frame.pack()
+
+style.configure("Rounded.TButton", borderwidth=0, relief="flat", padding=10, font=("Segoe UI", 10)) 
+
+root.mainloop()
+
+hash_file=Sha256_HashCalc(myfilepath)
 file_name=os.path.basename(myfilepath)
-storehash(p)
+storehash(hash_file)
